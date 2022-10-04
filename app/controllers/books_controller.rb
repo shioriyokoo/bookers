fd4e01409007ad0af2afab1cd671c51a6bc9a4f5
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2022-10-04 08:38:01 +0000

    [Remove] 3-課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,18 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :destroy, :edit, :update]
   def new
     @book = Book.new
   end
 
   def create
-    # 1.&2.データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3.データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4.詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :new
+    end
   end
 
   def index
@@ -25,14 +28,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to @book
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id]) # データ（レコード）を1件取得
     book.destroy # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books' # 投稿一覧画面へリダイレクト
   end
 
@@ -40,5 +49,9 @@ class BooksController < ApplicationController
   #  ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :body
+  end
 end
